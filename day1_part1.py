1447e7699b2d36bc1682141e6da748ef49bdb002
Mert Cobanov <mertcobanov@gmail.com>
2020-12-23 12:43:29 +0300

    main

--- day1_part1.py
+++ day1_part1.py
@@ -5,14 +5,20 @@
 
 from utils import read_input
 
-content = read_input("inputs/day1_input.txt")
-content_int = list(map(lambda x: int(x), content))
 
-for number in content_int:
-    if (2020 - number) in content_int:
-        print(f"For {number}, found {2020 - number}")
-        print(f"Result: {number * (2020 - number)}")
-        break
+def main():
+    content = read_input("inputs/day1_input.txt")
+    content_int = list(map(lambda x: int(x), content))
+
+    for number in content_int:
+        if (2020 - number) in content_int:
+            print(f"For {number}, found {2020 - number}")
+            print(f"Result: {number * (2020 - number)}")
+            break
+
+
+if __name__ == "__main__":
+    main()
 
 # For 1312, found 708
 # Result: 928896
--- day1_part2.py
+++ day1_part2.py
@@ -6,17 +6,21 @@
 
 from utils import read_input
 
-content = read_input("inputs/day1_input.txt")
-content_int = list(map(lambda x: int(x), content))
-content_sorted = sorted(content_int)
+def main():
+    content = read_input("inputs/day1_input.txt")
+    content_int = list(map(lambda x: int(x), content))
+    content_sorted = sorted(content_int)
 
-for index, first_number in enumerate(content_sorted):
-    for second_number in content_sorted[index:]:
-        calculated_number = first_number + second_number
-        if (2020 - calculated_number) in content_int:
-            print(f"For {first_number, second_number}, found {2020 - calculated_number}")
-            print(f"Result: {(2020 - calculated_number) * first_number * second_number}")
-            break
-            
+    for index, first_number in enumerate(content_sorted):
+        for second_number in content_sorted[index:]:
+            calculated_number = first_number + second_number
+            if (2020 - calculated_number) in content_int:
+                print(f"For {first_number, second_number}, found {2020 - calculated_number}")
+                print(f"Result: {(2020 - calculated_number) * first_number * second_number}")
+                break
+
+ if __name__ == "__main__":
+    main()
+
 # For (558, 664), found 798
 # Result: 295668576
