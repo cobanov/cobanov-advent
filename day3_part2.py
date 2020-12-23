01d471b4627291a9867a89828715456fb6ccc33c
Mert Cobanov <mertcobanov@gmail.com>
2020-12-23 14:32:19 +0300

    done

--- day3_part2.py
+++ day3_part2.py
@@ -4,26 +4,34 @@
 # puzzle answer: 1666768320
 
 from utils.utils import read_input
+from functools import reduce
 
 
 def main():
-
-    loc_x = 0
-    counter = 0
+    
+    step_sizes = ((1,1), (3, 1), (5, 1), (7, 1), (1, 2))
 
     content = read_input("inputs/day3_input.txt")
     length = len(content[0]) - 1
 
-    for line in content:
-        if loc_x >= length:
-            loc_x -= length
+    counter_results = list()
 
-        if line[loc_x] == "#":
-            counter += 1
+    for step_size_x, step_size_y in step_sizes:
+        counter = 0
+        loc_x = 0
+        for line in content[::step_size_y]:
+            if loc_x >= length:
+                loc_x -= length
 
-        loc_x += 3
+            if line[loc_x] == "#":
+                counter += 1
 
-    print(counter)
+            loc_x += step_size_x
+
+        counter_results.append(counter)
+    
+    print(counter_results)
+    print(reduce(lambda x, y: x*y, counter_results))
 
 
 if __name__ == "__main__":
